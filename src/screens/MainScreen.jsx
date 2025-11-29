daaef2869e5bb5fb7ba96183f31cdb2029923574
Dipu <dipwagh2345@gmail.com>
2025-11-29 17:42:00 +0530

    cta

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -13,6 +13,7 @@ const MainScreen = () => {
       <SlideText />
       <BlurRevealText />
       <Footer />
+      <button className="text-redy border border-redy py-2 px-4 rounded-md cursor-pointer fixed bottom-5 right-5 z-50 uppercase">prolingualhub</button>
     </div>
   )
 }
